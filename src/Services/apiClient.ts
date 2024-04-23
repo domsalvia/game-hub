d5c6230f06bf2a9adac072b92aa241982fdbcf8f
Dom Salvia <mymslive@nerdlogic.co.uk>
2024-04-23 14:37:28 +0100

    removed api key from git (old key invalidated)

--- src/Services/apiClient.ts
+++ src/Services/apiClient.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
+import apiSecrets from "./apiSecrets";
 
 export default axios.create({
     baseURL: 'https://api.rawg.io/api/',
     params: {
-        key: '6ded2faf58754ea18970b3c974361443'
+        key: apiSecrets.key
     }
 });
