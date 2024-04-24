132cad8c7d1d4071f24de3b83c5f01a39f88e209
Dom Salvia <mymslive@nerdlogic.co.uk>
2024-04-24 10:15:38 +0100

    highlight selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -10,10 +10,11 @@ import useGenres, { Genre } from "../Hooks/useGenres";
 import getCroppedImageUrl from "../Services/imageUrls";
 
 interface Props {
-  onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null,
+  onSelectGenre: (genre: Genre) => void
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -33,6 +34,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
               onClick={() => onSelectGenre(genre)}
               variant="link"
               fontSize="lg"
+              fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
             >
               {genre.name}
             </Button>
