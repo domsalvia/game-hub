0b53f8cf5ff4af315e699f7433862059586b3d80
Dom Salvia <mymslive@nerdlogic.co.uk>
2024-05-01 10:51:03 +0100

    simplify time calculations

--- src/Hooks/useGenres.ts
+++ src/Hooks/useGenres.ts
@@ -2,6 +2,7 @@ import { CACHE_KEY_GENRES } from "../data/constants";
 import { useQuery } from "@tanstack/react-query";
 import ApiClient from "../Services/apiClient";
 import genres from "../data/genres";
+import ms from 'ms';
 
 export interface Genre {
   id: number;
@@ -15,7 +16,7 @@ const useGenres = () =>
   useQuery({
     queryKey: CACHE_KEY_GENRES,
     queryFn: apiClient.getAll,
-    staleTime: (24*60*60*1000), //24h
+    staleTime: ms('24h'),
     initialData: genres 
   });
 
